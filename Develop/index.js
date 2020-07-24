25b3c949a674f4e6cc8de8c3d316ad087fb8d8c6
Jeffrey Harding <jeffreyh202@gmail.com>
2020-07-24 14:40:51 -0600

    questions and basic Read Me

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,57 @@
-var inq = require("inquirer");
-var fs = require("fs");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // array of questions for user
-const questions = [
-
-];
-
+const questions = inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your Title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "User Story AS A statement:",
+        name: "AS A"
+    },
+    {
+        type: "input",
+        message: "User Story I WANT statement:",
+        name: "I WANT"
+    },
+    {
+        type: "input",
+        message: "User Story SO THAT statement:",
+        name: "SO THAT"
+    },
+    {
+        type: "input",
+        message: "What are your installation instructions?",
+        name: "instructions"
+    },
+    {
+        type: "input",
+        message: "What is your project description?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Who are your contributors?",
+        name: "contributors"
+    }
+]).then(function(data) {
+    var filename = "README.md";
 // function to write README file
-function writeToFile(fileName, data) {
-}
+
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!");
+
+  });
+});
 
 // function to initialize program
 function init() {
