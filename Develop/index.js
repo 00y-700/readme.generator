05b6730d435c640519d720478512a24fca1010a8
Jeffrey Harding <jeffreyh202@gmail.com>
2020-07-25 15:58:54 -0600

    question cleanup

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// array of questions for user
-const question1 = {
-    type: "input",
-    message: "What is your Title?",
-    name: "Title"
-}
 
 inquirer.prompt([
     {
@@ -41,8 +35,23 @@ inquirer.prompt([
     },
     {
         type: "input",
+        message: "What is your License?",
+        name: "license"
+    },
+    {
+        type: "input",
         message: "Who are your contributors?",
         name: "contributors"
+    },
+    {
+        type: "input",
+        message: "What are your tests?",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What are your Questions?",
+        name: "qs"
     }
 ]).then((response)=>{
     fs.appendFileSync("README.md", "# " + response.title + '\n' + '\n', function(err) {
@@ -51,7 +60,7 @@ inquirer.prompt([
         }
         console.log("TITLE done");
     });
-    fs.appendFileSync("README.md", "## " + "User Story:" + '\n' + "* " + "AS A " + response.as + '\n', function(err) {
+    fs.appendFileSync("README.md", "## " + '<a name="user"></a>' + "User Story:" + '\n' + "* " + "AS A " + response.as + '\n', function(err) {
         if(err) {
             console.log(err);
         }
@@ -69,6 +78,13 @@ inquirer.prompt([
         }
         console.log("SO THAT done");
     });
+    fs.appendFileSync("README.md", "## " + "Table of Contents:" + '\n' + "* User Story" + '\n' + "* Installation" + '\n' + "* Description" + '\n' + 
+    "* License" + '\n' + "* Contributors" + '\n' + "* Tests" + '\n' + "* Questions" + '\n' + '\n', function(err) {
+        if(err) {
+            console.log(err);
+        }
+        console.log("TOC done");
+    });
     fs.appendFileSync("README.md", "## " + "Installation Instructions:" + '\n' + "* " + response.instructions + '\n' + '\n', function(err) {
         if(err) {
             console.log(err);
@@ -81,10 +97,28 @@ inquirer.prompt([
         }
         console.log("DESC done");
     });
+    fs.appendFileSync("README.md", "## " + "License:" + '\n' + "* " + response.license + '\n' + '\n', function(err) {
+        if(err) {
+            console.log(err);
+        }
+        console.log("LISC done");
+    });
     fs.appendFileSync("README.md", "## " + "Contributors:" + '\n' + "* " + response.contributors + '\n' + '\n', function(err) {
         if(err) {
             console.log(err);
         }
         console.log("CONT done");
+    });
+    fs.appendFileSync("README.md", "## " + "Tests:" + '\n' + "* " + response.test + '\n' + '\n', function(err) {
+        if(err) {
+            console.log(err);
+        }
+        console.log("TEST done");
+    });
+    fs.appendFileSync("README.md", "## " + "Questions:" + '\n' + "* " + response.qs + '\n' + '\n', function(err) {
+        if(err) {
+            console.log(err);
+        }
+        console.log("Qs done");
     });
 });
